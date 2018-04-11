0bad2bd18e722af93911c4ec45465a343ddb0020
Mathis Antony <sveitser@gmail.com>
2018-04-11 12:58:29 +0800

    python-language-server: 0.16.0 -> 0.17.1

--- pkgs/development/python-modules/python-language-server/default.nix
+++ pkgs/development/python-modules/python-language-server/default.nix
@@ -1,7 +1,8 @@
 { stdenv
 , jedi, mccabe, future, futures, rope, tox
 , yapf, pycodestyle, pluggy, pyflakes
-, json-rpc, pydocstyle, pytest
+, json-rpc, pydocstyle
+, autopep8, pytest, mock
 , fetchPypi
 , buildPythonPackage
 , pythonOlder
@@ -10,15 +11,23 @@
 buildPythonPackage rec {
 
   pname = "python-language-server";
-  version = "0.16.0";
+  version = "0.17.1";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "1sp9wyna44izw0ygb4m73v5g04fhqbpypkvfc7lqzyb22dp54a2a";
+    sha256 = "1hi5npffps5k1gwhgils1bbjhn78i1q4jzhh1v2kqjmc2ac4qmqf";
   };
 
   postPatch = stdenv.lib.optionalString (!pythonOlder "3.0") ''
-    substituteInPlace setup.py --replace 'configparser'  ""
+    substituteInPlace setup.py --replace "packages=find_packages(exclude=['contrib', 'docs', 'test'])" "packages=find_packages(exclude=['contrib', 'docs'])"
+    cat > test/__init__.py <<EOF
+# Copyright 2017 Palantir Technologies, Inc.
+import pytest
+from pyls import IS_WIN
+
+unix_only = pytest.mark.skipif(IS_WIN, reason="Unix only")
+windows_only = pytest.mark.skipif(not IS_WIN, reason="Windows only")
+EOF
   '';
 
   propagatedBuildInputs = [
@@ -34,10 +43,10 @@ buildPythonPackage rec {
     rope
   ] ++ stdenv.lib.optionals (pythonOlder "3.0") [ configparser futures ];
 
-  checkInputs = [ pytest ];
+  checkInputs = [ autopep8 mock pytest ];
 
   checkPhase = ''
-    py.test
+    py.test test
   '';
 
   meta = with stdenv.lib; {
